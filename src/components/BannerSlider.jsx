81eebaa01b33e80ee3a6e73a66f60b47c9a14df1
Maku Serrano <maku_serrano@outlook.com>
2024-05-23 13:14:12 -0300

    fix carrousel

--- src/components/BannerSlider.jsx
+++ src/components/BannerSlider.jsx
@@ -8,7 +8,6 @@ import img2 from "../assets/img/responsive/BannerSlider/main-second.webp";
 import img3 from "../assets/img/responsive/BannerSlider/main-third.webp";
 import { ReactComponent as Logo } from "../assets/logo/logo-sinfondo.svg";
 
-
 const SimpleSlider = () => {
   const settings = {
     dots: true,
@@ -16,6 +15,7 @@ const SimpleSlider = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    fade: true,
     arrows: false,
     responsive: [
       {
@@ -26,6 +26,7 @@ const SimpleSlider = () => {
           infinite: true,
           dots: true,
           arrows: false,
+          fade: true,
         },
       },
     ],
@@ -40,10 +41,8 @@ const SimpleSlider = () => {
           <img src={img1} className="slider__img__1" alt="img-slider-1" />
         </div>
         <div className="slider__img">
-          <img src={img2} className="slider__img__2" alt="img-slider-2"/>
-          <button className="slider__img__2__btn"> 
-            SHOP NOW
-          </button>
+          <img src={img2} className="slider__img__2" alt="img-slider-2" />
+          <button className="slider__img__2__btn">SHOP NOW</button>
         </div>
         <div className="slider__img">
           <img src={img3} className="slider__img__3" alt="img-slider-3" />
